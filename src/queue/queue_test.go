d60aa2c342609e471db806057ca7cdc9cddb5484
Liam Hendricks <liamh.blogg@gmail.com>
2022-01-06 15:21:52 -0700

    static queue for moving averages

--- src/queue/queue_test.go
+++ src/queue/queue_test.go
@@ -17,3 +17,19 @@ func TestQueue(t *testing.T) {
 	assert.Equal(t, 0, val)
 	assert.Equal(t, 2, q.Size())
 }
+
+func TestStaticQueue(t *testing.T) {
+	var q Queue = NewStaticQueue(10)
+	assert.Equal(t, true, q.Empty())
+	for i := 0; i < 10; i++ {
+		q.Enqueue(int64(i))
+	}
+	assert.Equal(t, false, q.Empty())
+	assert.Equal(t, q.Size(), 10)
+	q.Enqueue(int64(10))
+	// size should remain 10, popping off the last element
+	assert.Equal(t, q.Size(), 10)
+	val := q.Dequeue()
+	// popped element should not be 0, since it should have auto popped
+	assert.Equal(t, val, int64(1))
+}
---
+++ src/queue/static_queue.go
@@ -0,0 +1,57 @@
+package queue
+
+import "fmt"
+
+// this queue stays <= maxSize
+type StaticQueue struct {
+	values  []int64
+	maxSize int
+}
+
+func NewStaticQueue(length int) *StaticQueue {
+	return &StaticQueue{
+		values:  []int64{},
+		maxSize: length,
+	}
+}
+
+func (q *StaticQueue) Size() int {
+	return len(q.values)
+}
+
+func (q *StaticQueue) Empty() bool {
+	fmt.Printf("size: %d\n", q.Size())
+	return q.Size() == 0
+}
+
+func (q *StaticQueue) max() int {
+	return q.maxSize
+}
+
+func (q *StaticQueue) Dequeue() interface{} {
+	if !q.Empty() {
+		val := q.values[0]
+		q.values = q.values[1:]
+		return val
+	}
+
+	return -1
+}
+
+func (q *StaticQueue) Enqueue(n interface{}) {
+	if q.Size() == q.max() {
+		_ = q.Dequeue()
+	}
+	val, ok := n.(int64)
+	if ok {
+		q.values = append(q.values, val)
+	}
+}
+
+func (q *StaticQueue) Average() float32 {
+	var avg float32
+	for _, n := range q.values {
+		avg += float32(n)
+	}
+	return avg / float32(q.Size())
+}
